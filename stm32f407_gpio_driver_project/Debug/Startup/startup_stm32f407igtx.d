d32717f9957f7913e12fcdb57cab85a38cbdbf59
ramazan2765 <52526656+ramazan2765@users.noreply.github.com>
2024-01-06 14:18:22 +0100

    Add files via upload
    In this Project I created my own GPIO Project like HAL GPIO functions. You can use my GPIO functions to use GPIO configuration on your projects.

---
+++ stm32f407_gpio_driver_project/Debug/Startup/startup_stm32f407igtx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32f407igtx.o: ../Startup/startup_stm32f407igtx.s
---
+++ stm32f407_gpio_driver_project/Src/gpio.c
@@ -0,0 +1,110 @@
+/*
+ * gpio.c
+ *
+ *  Created on: Dec 18, 2023
+ *      Author: ramazan
+ */
+#include"gpio.h"
+
+void gpio_perip_clock_en(GPIO_RegDef_t* pGpiox)
+{
+	 if(pGpiox == GPIOA)
+		 GPIOA_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOB)
+		 GPIOB_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOC)
+		 GPIOC_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOD)
+		 GPIOD_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOE)
+		 GPIOE_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOF)
+		 GPIOF_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOG)
+		 GPIOG_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOH)
+		 GPIOH_CLOCK_ENABLE();
+	 else if(pGpiox == GPIOI)
+		 GPIOI_CLOCK_ENABLE();
+}
+
+uint8_t get_port_code(const GPIO_RegDef_t* pGpioX)
+{
+	  if(pGpioX == GPIOA)
+		  return GPIOA_PORT_CODE;
+	  else if(pGpioX == GPIOB)
+		  return GPIOB_PORT_CODE;
+	  else if(pGpioX == GPIOB)
+	  		  return GPIOB_PORT_CODE;
+	  else if(pGpioX == GPIOC)
+	  		  return GPIOC_PORT_CODE;
+	  else if(pGpioX == GPIOD)
+	  		  return GPIOD_PORT_CODE;
+	  else if(pGpioX == GPIOE)
+	  		  return GPIOE_PORT_CODE;
+	  else if(pGpioX == GPIOF)
+	  		  return GPIOF_PORT_CODE;
+	  else if(pGpioX == GPIOG)
+	  		  return GPIOG_PORT_CODE;
+	  else if(pGpioX == GPIOH)
+	  		  return GPIOH_PORT_CODE;
+}
+
+void gpio_init(GPIO_Handle_t* pGpio_handle)
+{
+	uint32_t temp=0;
+
+	//1.GPIO Clock ENABLE
+	gpio_perip_clock_en(pGpio_handle->pGpioX);
+
+	//2.GPIO pin modes configuration
+	temp=(pGpio_handle->gPIO_PingConfig.pin_mode<<(2*pGpio_handle->gPIO_PingConfig.pin_number)); // 01<<28
+	pGpio_handle->pGpioX->MODER &= ~(0b11 <<(2*pGpio_handle->gPIO_PingConfig.pin_number)); //register clear ettik
+	pGpio_handle->pGpioX->MODER |=temp;
+
+	//3. speed
+	temp=pGpio_handle->gPIO_PingConfig.pin_speed<<(2*pGpio_handle->gPIO_PingConfig.pin_number);
+	pGpio_handle->pGpioX->OSPEEDR &= ~(0b11<<(2*pGpio_handle->gPIO_PingConfig.pin_number));
+	pGpio_handle->pGpioX->OSPEEDR |= temp;
+
+	//4. pull_up pull_down
+	temp=pGpio_handle->gPIO_PingConfig.pin_pupd<<(2*pGpio_handle->gPIO_PingConfig.pin_number);
+	pGpio_handle->pGpioX->PUPDR &= ~(0b11 <<(2*pGpio_handle->gPIO_PingConfig.pin_number));
+	pGpio_handle->pGpioX->PUPDR |= temp;
+	 //5.otype
+	temp=pGpio_handle->gPIO_PingConfig.pin_otyper<<(pGpio_handle->gPIO_PingConfig.pin_number);
+	pGpio_handle->pGpioX->OTYPER &= ~(0b1<<pGpio_handle->gPIO_PingConfig.pin_number);
+	pGpio_handle->pGpioX->OTYPER |= temp;
+	  //6.alternate mode
+
+}
+
+void gpio_writeto_output_pin(GPIO_RegDef_t* pGpiox, uint8_t pin_no, uint8_t val)
+{
+	if(val==GPIO_PIN_SET)
+		pGpiox->ODR |= (1u<<pin_no);
+	else
+		pGpiox->ODR &= ~(1u << pin_no);
+}
+
+void gpio_toggleto_output_pin(GPIO_RegDef_t* pGpiox,uint8_t pin_no)
+{
+	pGpiox->ODR ^= (1u<<pin_no);
+}
+
+void gpio_writeto_output_port(GPIO_RegDef_t*pGpiox,uint16_t val)
+{
+	pGpiox->ODR=val;
+}
+
+uint32_t gpio_read_input_pin(GPIO_RegDef_t* pGpiox, uint8_t pin_no)
+{
+	uint16_t res=0;
+
+	res=(pGpiox->IDR & (1u<<pin_no));
+	return res != 0 ? GPIO_PIN_SET : GPIO_PIN_RESET;
+}
+
+
+
+
